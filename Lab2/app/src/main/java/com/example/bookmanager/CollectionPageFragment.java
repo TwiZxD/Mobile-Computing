74e41dcb631e7bf7db3cdff5697ed2ba3fde4aea
TwiZxD <iamtwiz@gmail.com>
2018-10-07 16:14:55 +0200

    added share preferences

--- Lab2/app/src/main/java/com/example/bookmanager/CollectionPageFragment.java
+++ Lab2/app/src/main/java/com/example/bookmanager/CollectionPageFragment.java
@@ -20,8 +20,6 @@ public class CollectionPageFragment extends Fragment implements AdapterView.OnIt
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //if (getArguments() != null) { }
     }
 
     @Override
